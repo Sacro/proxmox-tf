0f89bf874fbd8a037afea5f6802f6c876196a15c
Ben Woodward <ben@benwoodward.me.uk>
2024-05-03 22:19:20 +0100

    feat: don't use dns cache for pods

--- talos/configuration.tf
+++ talos/configuration.tf
@@ -176,8 +176,8 @@ locals {
       # }
       features = {
         hostDNS = {
-          enabled              = true
-          forwardKubeDNSToHost = true
+          enabled = true
+          # forwardKubeDNSToHost = true
         }
       }
       files = [{
