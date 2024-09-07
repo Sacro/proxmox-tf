794d34334214368c20497d1d46c83d91baee20f7
Ben Woodward <ben@benwoodward.me.uk>
2024-09-07 18:46:41 +0100

    feat: upgrade fluxcd provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     flux = {
       source  = "fluxcd/flux"
-      version = "~>1.2.3"
+      version = "~>1.3.0"
     }
     github = {
       source  = "integrations/github"
